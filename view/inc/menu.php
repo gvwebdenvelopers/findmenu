970bfbd5617ee27d1f281845f9f48ce90e912ea7
Oscar Otero <oscar.otero.millan@gmail.com>
2016-11-26 07:53:53 +0100

    añadidos head con metas (includes por revisar), menu (de otra plantilla; falta adaptar css) t footer

--- view/inc/menu.php
+++ view/inc/menu.php
@@ -1,44 +1,29 @@
-<div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="<?php friendly('?module=home'); ?>"><img src="<?php echo IMG_PATH ?>logo.png" alt="logo"></a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="index.php?module=home">Home</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products <i class="icon-angle-down"></i></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="<?php friendly('?module=products_fe&function=create_products'); ?>">Create</a></li>
-                            <li><a href="<?php friendly('?module=products_fe&function=list_products'); ?>">View</a></li>
-                        </ul>
-                    </li>
-                    <!--
-                    <li class= <?php/* if($_GET['module'] == "products"){ echo "active";}else{ echo "";} */?> >
-                        <a href="index.php?module=products&view=create_products">Products</a>
-                    </li>
-                    -->
-                    <li>
-                        <a href="index.php?module=pricing">Pricing</a>
-                    </li>
-                    <li>
-                        <a href="index.php?module=services">Services</a>
-                    </li>
-                    <li>
-                        <a href="index.php?module=portfolio">Portfolio</a>
-                    </li>
-                    <li>
-                        <a href="index.php?module=contact">Contact</a>
-                    </li>
-
-                </ul>
-            </div>
-        </div>
-    </header><!--/header-->
+<!-- header menu -->
+<header id="fh5co-header-section" class="sticky-banner">
+  <div class="container">
+    <div class="nav-header">
+      <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+      <h1 id="fh5co-logo"><a href="index.html">Find Menu</a></h1>
+      <!-- START #fh5co-menu-wrap -->
+      <nav id="fh5co-menu-wrap" role="navigation">
+        <ul class="sf-menu" id="fh5co-primary-menu">
+          <li class="active"><a href="index.html">Home</a></li>
+          <li>
+            <a href="#" class="fh5co-sub-ddown">Dropdown</a>
+            <ul class="fh5co-sub-menu">
+              <li><a href="#">Web Design</a></li>
+              <li><a href="#">CSS3 &amp; HTML5</a></li>
+              <li><a href="#">Angular JS</a></li>
+              <li><a href="#">Node JS</a></li>
+              <li><a href="#">Django &amp; Python</a></li>
+            </ul>
+          </li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
+</section>
