1b13740121a1ef7a89eb32687a68ef93998fd7da
Jorge <jordimart83@gmail.com>
2016-11-30 00:49:28 +0100

    Subida carpeta media y fotolupa para search. Aseo algo el código tocado

--- view/inc/header.php
+++ view/inc/header.php
@@ -40,6 +40,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> 
     <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     
   </head>
